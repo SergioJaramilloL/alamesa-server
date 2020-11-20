1bc23d0bf429a9a102ca16a77e470c9626596439
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-11-20 10:18:24 -0500

    Fix structure for repo, delete file yarn-error change nodemon to devdependencies

--- src/models/client.model.js
+++ src/models/client.model.js
@@ -1,11 +1,11 @@
 const { Schema, model } = require('mongoose');
 
 const clientSchema = new Schema ({
-  name: 'String',
-  lastName: 'String',
-  password: 'String',
-  email: 'String',
-  identification: 'String',
+  name: String,
+  lastName: String,
+  password: String,
+  email: String,
+  identification: String,
   
   },{
     timestamps: true,
