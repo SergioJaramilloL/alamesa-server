f4f5ae5a2ac896db650dfcdf9e950edd3de6709c
andress20 <andress20@gmail.com>
2020-11-30 10:59:34 -0500

    add authentication endpoint List

--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -4,7 +4,7 @@ const { auth } = require('../utils/auth')
 
 router.route('/sign-up').post(restaurantController.signup);
 router.route('/sign-in').post(restaurantController.signin);
-router.route('/').get(restaurantController.list);
+router.route('/').get(auth, restaurantController.list);
 router.route('/profile').get(auth, restaurantController.show);
 router.route('/:restaurantId').put(restaurantController.update);
 router.route('/:restaurantId').delete(restaurantController.destroy);
