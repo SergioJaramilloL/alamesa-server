03f0b594acf578d8c0b3edcae43338c038a7b691
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-11-30 12:01:11 -0500

    created model and controller for sanitary register

---
+++ src/models/sanitaryRegister.model.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const sanitaryRegisterSchema = new Schema ({
+  temperature: {
+    type: String,
+    required: true,
+  },
+  question1SymptomsCovid: {
+    type: Boolean,
+  },
+  question2ContactWithPeople: {
+    type: Boolean,
+  },
+  question3InternationalTravel: {
+    type: Boolean,
+  },
+  question4HealthWorker: {
+    type: Boolean,
+  },
+  client: {
+    type: { type: Schema.Types.ObjectId, ref: 'Client'},
+    required: true,
+  },
+  reservation: {
+    type: { type: Schema.Types.ObjectId, ref: 'Reservation'},
+    required: true,
+  },
+  companion: {
+    type: { type: Schema.Types.ObjectId, ref: 'Companion'},
+  },
+}, {
+  timestamps: true,
+});
+
+const SanitaryRegister = model('SanitaryRegister', sanitaryRegisterSchema);
+
+module.exports = SanitaryRegister;
