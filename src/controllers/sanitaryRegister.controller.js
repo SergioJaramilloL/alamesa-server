b6f1d9e38909c772e39144df92fa3a46d43a6518
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-12-02 11:13:23 -0500

    aggregated update for sanitary register in controller

--- src/controllers/sanitaryRegister.controller.js
+++ src/controllers/sanitaryRegister.controller.js
@@ -4,16 +4,21 @@ const Client = require('../models/client.model');
 module.exports = {
   async show(req, res) {
     const { sanitaryRegisterId } = req.params;
-
-    const sanitaryRegister = await SanitaryRegister
-      .findById(sanitaryRegisterId)
-      .populate({ path: 'user', select: 'name'})
-    
-    res.status(200).json(sanitaryRegister)
+    try {
+      const sanitaryRegister = await SanitaryRegister
+        .findById(sanitaryRegisterId)
+        .populate({ path: 'user', select: 'name'})
+      if(!sanitaryRegister) {
+        throw new Error('Sanitary register not exist')
+      }
+      res.status(200).json(sanitaryRegister)
+    }
+    catch(err) {
+      res.states(400).json({ message: 'Sanitary register not found'})
+    }
   },
   async create(req, res) {
     try {
-      console.log(req.client)
       const client = await Client.findById(req.client)
       if(!client) {
         throw new Error('Invalid client')
@@ -26,6 +31,20 @@ module.exports = {
       res.status(201).json(sanitaryRegister)
     } catch(err) {
       res.status(400).json({ message: err.message })
+    }
+  },
+  async update(req, res) {
+    try {
+      const { sanitaryRegisterId } = req.params;
+      const sanitaryRegister = await SanitaryRegister.findByIdAndUpdate(sanitaryRegisterId, req.body, { new: true })
+
+      if(!sanitaryRegister) {
+        throw new Error('Could not update that sanitary register')
+      }
+      res.status(200).json({ message: 'Sanitary register updated', data: sanitaryRegister })
+    }
+    catch(err) {
+      res.status(400).json({ message: 'Sanitary register could not be updated' })
     }
   }
 }
