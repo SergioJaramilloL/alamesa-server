70fb56c767508e8696cdc917ffc34c383b626985
Carolina Salazar <65572735+carosalazar28@users.noreply.github.com>
2020-12-07 17:48:04 -0500

    add env for mongo_uri (#23)

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
-function connect(){
-  mongoose.connect('mongodb://localhost:27017/alamesa', {
+function connect() {
+  mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  mongoose.connection.once('open', () => 
+  mongoose.connection.once('open', () =>
     console.log('connection established sucessfully')
   );
-  mongoose.connection.on('error', (err) => 
+  mongoose.connection.on('error', (err) =>
     console.log('something went wrong', err)
   );
   return mongoose.connection;
