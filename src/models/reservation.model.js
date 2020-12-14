9b2468574b1a00b3bb3a2e9595012c6f62894fe8
danielsespitia <63252057+danielsespitia@users.noreply.github.com>
2020-12-14 16:16:24 -0500

    Feature bck004 (#29)
    * fixes before merging develop
    
    * updated Client and Restaurant models
    
    * fixed requests from PR
    
    * fixed PUT methods conflicts
    
    * fixed destroy controller to link properly with front end
    
    * fixed controllers validation
    
    * reverted changed from clientcontroller
    
    * resolved conflicts
    
    * solved conflicts in routes
    
    * created reservation model view controller
    
    * added recent changes from develop
    
    * added reservation model/view/controllers
    
    * added try catch and auth
    
    * controller update

---
+++ src/models/reservation.model.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const reservationSchema = new Schema ({
+  branch: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  range: {
+    type: Number,
+  },
+  people: {
+    type: Number,
+  },
+  paymentMethod: {
+    type: String,
+  },
+  status: {
+    type: String,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'Client',
+    required: true,
+  },
+  provider: {
+    type: Schema.Types.ObjectId,
+    ref: 'Restaurant',
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
+
+const Reservation = model('Reservation', reservationSchema);
+
+module.exports = Reservation;
---
+++ src/routes/reservation.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const reservationController = require('../controllers/reservation.controller');
+const { auth } = require('../utils/auth');
+
+router.route('/:restaurantId/:clientId').post(auth, reservationController.create);
+router.route('/restaurant/:reservationId').get(auth, reservationController.showRestaurant);
+router.route('/client/:reservationId').get(auth, reservationController.showClient);
+router.route('/:reservationId').delete(auth, reservationController.destroy);
+
+module.exports = router
