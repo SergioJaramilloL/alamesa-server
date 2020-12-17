92afe2b0807e0e50d8590c50e90561dad746df0f
andress20 <andress20@gmail.com>
2020-12-17 11:50:50 -0500

    delete idClient parameter from post reservation endpoint

--- src/routes/reservation.js
+++ src/routes/reservation.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const reservationController = require('../controllers/reservation.controller');
 const { auth } = require('../utils/auth');
 
-router.route('/:restaurantId/:clientId').post(auth, reservationController.create);
+router.route('/:restaurantId').post(auth, reservationController.create);
 router.route('/restaurant/:reservationId').get(auth, reservationController.showRestaurant);
 router.route('/client/:reservationId').get(auth, reservationController.showClient);
 router.route('/:reservationId').delete(auth, reservationController.destroy);
