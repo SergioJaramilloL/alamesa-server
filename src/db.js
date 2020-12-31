821f4f0869e9a5962fde48fab8ea9c3832444590
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-12-31 18:44:38 -0500

    add env for uri conect with mongo atlas

--- src/db.js
+++ src/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 function connect() {
-  mongoose.connect(process.env.MONGO_URI, {
+  const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/alamesa';
+  const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
+  };
+
+  mongoose.connect(mongoURI, options);
+  
   mongoose.connection.once('open', () =>
     console.log('connection established sucessfully')
   );
