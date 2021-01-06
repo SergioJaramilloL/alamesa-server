727f2efeccb540359e23cff062c6d591dc504a3a
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-06 16:32:29 -0500

    created the MVC from dishes

---
+++ src/controllers/dish.controller.js
@@ -0,0 +1,50 @@
+const { create, update } = require('../models/dish.model');
+const Dish = require('../models/dish.model');
+const Menu = require('../models/menu.model');
+const Restaurant = require('../models/restaurant.model');
+
+module.exports = {
+
+  async create(req, res) {
+    try {
+      const restaurant = await Restaurant.findById(req.restaurant)
+      if(!restaurant) {
+        throw new Error('Invalid restaurant')
+      }
+
+      const menu = restaurant.menu
+      if(!menu) {
+        menu = await Menu.create({
+          provider: restaurant,
+        })
+      }
+
+      const dish = await Dish.create({ ...req.body, })
+
+      menu.dish.push(dish); 
+      await menu.save({ validateBeforeSave: false })
+
+      restaurant.menu = menu;
+      await restaurant.save({ validateBeforeSave: false })
+
+      res.status(201).json(menu)
+    } catch(err) {
+      res.status(400).json({ message: err.message })
+    }
+  },
+
+  async update(req, res) {
+    try {
+      const { dishId } = req.params;
+
+      const dish = await Dish
+        .findByIdAndUpdate(dishId, req.body, { new: true, useFindAndModify: false, })
+      if(!dish) {
+        throw new Error('Could not update that dish')
+      }
+      res.status(200).json({ message: 'Dish updated', data: dish })
+    } catch(err) {
+      res.status(400).json({ message: 'Dish could not be updated' })
+    }
+  },
+}
--- src/models/dish.model.js
+++ src/models/dish.model.js
@@ -1,12 +1,26 @@
 const { Schema, model } = require('mongoose');
 
 const dishSchema = new Schema ({
+  nameDish: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  description: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
   menu: {
     type: Schema.Types.ObjectId,
     ref:'Menu',
     required: true,
   },
-
 },{
   timestamps: true,
 });
